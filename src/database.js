82e4f893fbc8431db967e54e857b72a4d17d932d
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2024-12-19 23:11:18 +0000

    Implement the get, save and remove functions of the database (the local storage)

--- src/database.js
+++ src/database.js
@@ -1,11 +1,21 @@
 function get (listname) {
-    return JSON.parse(localStorage.get(listname));
+    return JSON.parse(localStorage.getItem(listname)) || [];
 }
 
 function save (listname, listobject) {
-    localStorage.setItem(listname, JSON.stringify(listobject));
+    let tempList = JSON.parse(localStorage.getItem(listname)) || [];
+    tempList.push(listobject);
+    localStorage.setItem(listname, JSON.stringify(tempList));
+
+    let lastId = parseInt(localStorage.getItem(`${listname}_lastId`)) || 0;
+    localStorage.setItem(`${listname}_lastId`, lastId + 1);
 }
 
 function remove(listname) {
-    localStorage.removeItem(listname)
+    localStorage.removeItem(listname);
+    localStorage.removeItem(`${listname}_lastId`);
+}
+
+export default database = {
+    get, save, remove
 }
